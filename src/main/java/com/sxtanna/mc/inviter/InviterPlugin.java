2b71f2c19250264b557467793cc12760bf354ea1
Sxtanna <ranald.taylor@gmail.com>
2020-05-27 14:28:42 -0400

    initial

---
+++ src/main/java/com/sxtanna/mc/inviter/InviterPlugin.java
@@ -0,0 +1,26 @@
+package com.sxtanna.mc.inviter;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class InviterPlugin extends JavaPlugin
+{
+
+	@Override
+	public void onLoad()
+	{
+
+	}
+
+	@Override
+	public void onEnable()
+	{
+
+	}
+
+	@Override
+	public void onDisable()
+	{
+
+	}
+
+}
